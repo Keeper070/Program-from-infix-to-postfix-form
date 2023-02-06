184003a3b5d0826fbd17e6e535fa1ed87597ff3f
Keeper070 <99165594+Keeper070@users.noreply.github.com>
2023-02-06 22:44:36 +0400

    Добавил текст на первую форму

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,11 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Timers.Timer();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.timer1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,21 +48,13 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(501, 83);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(287, 20);
-            this.textBox1.TabIndex = 2;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.label1.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.label1.ForeColor = System.Drawing.SystemColors.WindowText;
-            this.label1.Location = new System.Drawing.Point(501, 83);
+            this.label1.Location = new System.Drawing.Point(501, 106);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(287, 23);
             this.label1.TabIndex = 3;
@@ -70,7 +63,7 @@
             // textBox2
             // 
             this.textBox2.BackColor = System.Drawing.SystemColors.Window;
-            this.textBox2.Location = new System.Drawing.Point(104, 83);
+            this.textBox2.Location = new System.Drawing.Point(102, 109);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(287, 20);
@@ -84,30 +77,52 @@
             this.timer1.SynchronizingObject = this;
             this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Elapsed);
             // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(501, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(351, 22);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Входная строка (в инфиксной форме)";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(102, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(269, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Входная строка (в постфиксной форме)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Info;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.timer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.Label label2;
 
         private System.Timers.Timer timer1;
 
         private System.Windows.Forms.TextBox textBox2;
 
         private System.Windows.Forms.Label label1;
-
-        private System.Windows.Forms.TextBox textBox1;
 
         private System.Windows.Forms.Button button1;
 
--- Form1.cs
+++ Form1.cs
@@ -48,12 +48,20 @@ namespace LR_1
         }
 
         #endregion
-
+        
+        private void Translation()
+        {
+            
+        }
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
         {
         }
     }
